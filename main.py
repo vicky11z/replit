cdec6c92272108fda93cf8abd58e5571e9bf6070
vicky11z <vicky11z@berkeley.edu>
2021-12-29 21:13:03 -0800

    Unit-103-Printing-and-Variables

---
+++ main.py
@@ -0,0 +1,14 @@
+# DO NOW
+from DoNow import theNow
+
+# Part 2 - delete the hash on the line below and click "Run". What do you see? What does this have to do with lines 14 & 15 on the DoNow.py file?
+#theNow()
+
+# LAB
+from Lab import part3, part4
+
+# Uncomment below to run part 3
+#part3()
+
+# Uncomment below to run part 4
+#part4()
